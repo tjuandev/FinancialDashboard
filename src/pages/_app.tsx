8a55051833e6dc7164a4cded4091b66d815ebbf0
tjuandev <tpydeveloper@gmail.com>
2022-10-03 15:42:27 -0300

    feat: Adding Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,17 @@ const App = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="A simple project starter to Typescript, React, NextJS and Styled Components"
         />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
