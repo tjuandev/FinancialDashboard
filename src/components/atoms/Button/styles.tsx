dddb84bd6ac2f6dc5d120c4222ce62460a935363
tjuandev <tpydeveloper@gmail.com>
2022-10-05 11:44:00 -0300

    feat: improving Button styled

--- src/components/atoms/Button/styles.tsx
+++ src/components/atoms/Button/styles.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+
+import { onHoverInvertButtonColors } from 'styles/mixins'
 import theme from 'theme'
 
 type ContainerProps = {
@@ -9,8 +11,11 @@ export const Container = styled.button<ContainerProps>`
   padding: ${({ padding }) => (padding ? padding : theme.spacing['2'])};
   border-radius: ${theme.borderRadius.base};
 
-  background: ${theme.colors.secondary};
-
   font-size: 1rem;
   font-weight: ${theme.typography.fontWeights.semibold};
+
+  transition: ${theme.transitions.base};
+  cursor: pointer;
+
+  ${onHoverInvertButtonColors(theme.colors.secondary, theme.colors.primary)}
 `
