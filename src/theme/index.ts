6b77a0f497327201a9bb208bef09907bf06b0a34
tjuandev <tpydeveloper@gmail.com>
2022-10-05 11:43:47 -0300

    feat: adding animation config to theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,12 +2,14 @@ import colors from './colors'
 import typography from './typography'
 import borderRadius from './borderRadius'
 import spacing from './spacing'
+import { transitions } from './animation'
 
 const theme = {
   colors,
   typography,
   borderRadius,
-  spacing
+  spacing,
+  transitions
 }
 
 export default theme
