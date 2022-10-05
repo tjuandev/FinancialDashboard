1fec4c89c5f2fadcb820059bfb483552c0642cc8
tjuandev <tpydeveloper@gmail.com>
2022-10-05 11:19:19 -0300

    feat: adding spacing config to theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,13 @@
 import colors from './colors'
 import typography from './typography'
 import borderRadius from './borderRadius'
+import spacing from './spacing'
 
 const theme = {
   colors,
   typography,
-  borderRadius
+  borderRadius,
+  spacing
 }
 
 export default theme
