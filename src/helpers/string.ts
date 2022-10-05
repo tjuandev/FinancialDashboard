69fee8f49e8d78939965b1f064f5e9fc64570150
tjuandev <tpydeveloper@gmail.com>
2022-10-05 15:25:22 -0300

    refactor: Changing function to getFirstAndLastInitials

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,1 +1,15 @@
-export const getInitialUpperCase = (str: string) => str[0].toUpperCase()
+export const getFirstAndLastInitialsUpperCase = (str: string) => {
+  if (!str) {
+    return ''
+  }
+
+  const firstInitialRegex = /(\b\S)?/g
+  const lastInitialRegex = /(^\S|\S$)?/g
+
+  return str
+    ?.match(firstInitialRegex)
+    ?.join('')
+    ?.match(lastInitialRegex)
+    ?.join('')
+    ?.toUpperCase()
+}
