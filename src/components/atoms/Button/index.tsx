37a1732d61fe06e8e685b75d494758772681cb95
tjuandev <tpydeveloper@gmail.com>
2022-10-06 13:13:46 -0300

    feat: Improving Button component

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,14 +1,30 @@
-import { MouseEventHandler } from 'react'
+import { MouseEventHandler, ReactNode, CSSProperties } from 'react'
 import * as S from './styles'
 
 type Props = {
-  children: React.ReactNode
+  children: ReactNode
   onClick?: MouseEventHandler
   padding?: string
+  width?: string
+  height?: string
+  buttonStyles?: CSSProperties
 }
 
-const Button = ({ children, onClick, padding }: Props) => (
-  <S.Container onClick={onClick} padding={padding}>
+const Button = ({
+  children,
+  onClick,
+  padding,
+  height = '40px',
+  width = '40px',
+  buttonStyles
+}: Props) => (
+  <S.Container
+    height={height}
+    width={width}
+    onClick={onClick}
+    padding={padding}
+    style={buttonStyles}
+  >
     {children}
   </S.Container>
 )
--- src/components/atoms/Button/styles.tsx
+++ src/components/atoms/Button/styles.tsx
@@ -5,11 +5,16 @@ import theme from 'theme'
 
 type ContainerProps = {
   padding?: string
+  width: string
+  height: string
 }
 
 export const Container = styled.button<ContainerProps>`
   padding: ${({ padding }) => (padding ? padding : theme.spacing['2'])};
   border-radius: ${theme.borderRadius.base};
+
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
 
   font-size: 1rem;
   font-weight: ${theme.typography.fontWeights.semibold};
