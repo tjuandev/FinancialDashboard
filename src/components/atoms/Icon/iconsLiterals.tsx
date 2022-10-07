021e2a2594e74328be4a9a5cdc2c9be5fc0f6aef
tjuandev <tpydeveloper@gmail.com>
2022-10-07 14:58:33 -0300

    feat: Adding icons

--- src/components/atoms/Icon/iconsLiterals.tsx
+++ src/components/atoms/Icon/iconsLiterals.tsx
@@ -1,9 +1,13 @@
 import Transactions from './icons/Transactions'
 import Notification from './icons/Notification'
 import Wallet from './icons/Wallet'
+import StockUp from './icons/StockUp'
+import StockDown from './icons/StockDown'
 
 export const iconsLiterals = {
   transactions: <Transactions />,
   notification: <Notification />,
-  wallet: <Wallet />
+  wallet: <Wallet />,
+  'stock-up': <StockUp />,
+  'stock-down': <StockDown />
 }
