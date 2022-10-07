81df5936001f0569d3f41b91ce04d8baa3d3146c
tjuandev <tpydeveloper@gmail.com>
2022-10-07 16:47:31 -0300

    feat: Improving styled of Grid component

--- src/components/atoms/Grid/index.tsx
+++ src/components/atoms/Grid/index.tsx
@@ -8,6 +8,9 @@ const Grid = styled.div<GridProps>`
   gap: ${({ gap = 2 }) => theme.spacing[gap!]};
   grid-template-rows: ${({ rows }) => rows};
   grid-template-columns: ${({ columns }) => columns};
+
+  width: 100%;
+  height: 100%;
 `
 
 export default Grid
--- src/components/atoms/Grid/types.ts
+++ src/components/atoms/Grid/types.ts
@@ -1,9 +1,9 @@
 import theme from 'theme'
 
-type SpacingInferenceType = typeof theme['spacing']
+export type SpacingInferenceTypeOptions = keyof typeof theme['spacing']
 
 export type GridProps = {
   rows?: number | string
   columns?: number | string
-  gap?: keyof SpacingInferenceType
+  gap?: SpacingInferenceTypeOptions
 }
