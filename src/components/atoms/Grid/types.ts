06fd848e236292d6d16b452b0c698279094b9ddd
tjuandev <tpydeveloper@gmail.com>
2022-10-07 15:11:16 -0300

    refactor: making Grid props be optional

--- src/components/atoms/Grid/types.ts
+++ src/components/atoms/Grid/types.ts
@@ -3,7 +3,7 @@ import theme from 'theme'
 type SpacingInferenceType = typeof theme['spacing']
 
 export type GridProps = {
-  rows: number | string
-  columns: number | string
+  rows?: number | string
+  columns?: number | string
   gap?: keyof SpacingInferenceType
 }
