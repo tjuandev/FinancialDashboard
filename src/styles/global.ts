e446531f31f214faab72b00623feb541675ba5c6
tjuandev <tpydeveloper@gmail.com>
2022-10-15 17:58:16 -0300

    feat: Adding Inter font to the project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
   input,
   button,
   text-area {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   body, html, #__next {
