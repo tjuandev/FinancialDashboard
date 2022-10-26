5198f0ba402f72200aaf424723d92a4adbb8d22b
tjuandev <tpydeveloper@gmail.com>
2022-10-26 20:26:54 -0300

    refactor: removing unused code

--- src/components/molecules/Kpi/__tests__/Kpi.test.tsx
+++ src/components/molecules/Kpi/__tests__/Kpi.test.tsx
@@ -6,12 +6,6 @@ import Kpi from '..'
 const kpiName = 'R$ 1290,20'
 const kpiDescription = 'Seu saldo atual'
 
-const iconTestName = 'icon-test'
-
-jest.mock('components/atoms', () => ({
-  Icon: () => <svg aria-label={iconTestName} />
-}))
-
 describe('<Kpi />', () => {
   it('Should render name and description if props passed', () => {
     render(<Kpi name={kpiName} description={kpiDescription} />)
