84d300bcefeecc7e687170afa76951db6609f121
tjuandev <tpydeveloper@gmail.com>
2022-10-26 19:21:26 -0300

    feat: Improving ExpandableInput component

--- src/components/molecules/ExpandableInput/index.tsx
+++ src/components/molecules/ExpandableInput/index.tsx
@@ -1,28 +1,53 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
-import { Button, Icon } from 'components/atoms'
+import { Button as BaseButton, Icon } from 'components/atoms'
 
 import * as S from './styles'
 import { ExpandableInputProps } from './types'
+import { useClickOutside } from 'hooks'
+
+const Button = () => {
+  return (
+    <S.ButtonWrapper>
+      <BaseButton width="44px" height="100%" __hover={() => ({})}>
+        <Icon name="search" />
+      </BaseButton>
+    </S.ButtonWrapper>
+  )
+}
 
 const ExpandableIconButton = ({ onOpen, size }: ExpandableInputProps) => {
   const [open, setOpen] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  const className = open ? 'open' : ''
+  const openClassName = open ? 'open' : ''
+  const isExpanded = open ? 'true' : 'false'
 
   const handleOpen = () => {
     if (onOpen) onOpen()
     setOpen(true)
+
+    inputRef?.current?.focus()
   }
 
+  const handleClose = () => setOpen(false)
+  const wrapperRef = useClickOutside<HTMLDivElement>({ onClick: handleClose })
+
   return (
-    <S.Wrapper className={className} onClick={handleOpen} size={size}>
-      <S.ButtonWrapper>
-        <Button width="44px" height="100%" __hover={() => ({})}>
-          <Icon name="notification" />
-        </Button>
-      </S.ButtonWrapper>
-      <S.Input className={className} type="text" placeholder="Search..." />
+    <S.Wrapper
+      ref={wrapperRef}
+      className={openClassName}
+      onClick={handleOpen}
+      aria-expanded={isExpanded}
+      size={size}
+    >
+      <Button />
+      <S.Input
+        className={openClassName}
+        type="text"
+        placeholder="Search..."
+        ref={inputRef}
+      />
     </S.Wrapper>
   )
 }
