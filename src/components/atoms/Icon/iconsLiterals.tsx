6346a98f26096611b7122cb31dbea0297f37b6a9
tjuandev <tpydeveloper@gmail.com>
2022-10-26 19:02:59 -0300

    feat: adding Search Icon

--- src/components/atoms/Icon/iconsLiterals.tsx
+++ src/components/atoms/Icon/iconsLiterals.tsx
@@ -4,6 +4,7 @@ import Wallet from './icons/Wallet'
 import StockUp from './icons/StockUp'
 import StockDown from './icons/StockDown'
 import ChevronDown from './icons/ChevronDown'
+import Search from './icons/Search'
 
 export const iconsLiterals = {
   transactions: <Transactions />,
@@ -11,5 +12,6 @@ export const iconsLiterals = {
   wallet: <Wallet />,
   'stock-up': <StockUp />,
   'stock-down': <StockDown />,
-  'chevron-down': <ChevronDown />
+  'chevron-down': <ChevronDown />,
+  search: <Search />
 }
