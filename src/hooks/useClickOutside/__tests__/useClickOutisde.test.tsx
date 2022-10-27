d84612dcadfdfa6300f12fa28306d488d508dddf
tjuandev <tpydeveloper@gmail.com>
2022-10-27 20:16:05 -0300

    feat: Improving coverage of useClickOutside

--- src/hooks/useClickOutside/__tests__/useClickOutisde.test.tsx
+++ src/hooks/useClickOutside/__tests__/useClickOutisde.test.tsx
@@ -1,13 +1,16 @@
 import { Ref } from 'react'
 
-import { render, renderHook } from '@testing-library/react'
+import { render, renderHook, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import useClickOutside from '..'
 
-const MockComponent = ({ mockRef }: { mockRef: Ref<HTMLDivElement> }) => (
-  <div ref={mockRef} />
+const MockComponent = ({ mockRef }: { mockRef: Ref<HTMLButtonElement> }) => (
+  <button ref={mockRef} />
 )
+
+const CALLED_ONCE = 1
+const NEVER_CALLED = 0
 
 describe('useClickOutside', () => {
   const user = userEvent.setup()
@@ -16,13 +19,29 @@ describe('useClickOutside', () => {
     const onClick = jest.fn()
 
     const { result } = renderHook(() =>
-      useClickOutside<HTMLDivElement>({ onClick })
+      useClickOutside<HTMLButtonElement>({ onClick })
     )
 
     render(<MockComponent mockRef={result.current} />)
 
     await user.click(document.body)
 
-    expect(onClick).toBeCalledTimes(1)
+    expect(onClick).toBeCalledTimes(CALLED_ONCE)
+  })
+
+  it('Should not call onClick when user click in the component referenced', async () => {
+    const onClick = jest.fn()
+
+    const { result } = renderHook(() =>
+      useClickOutside<HTMLButtonElement>({ onClick })
+    )
+
+    render(<MockComponent mockRef={result.current} />)
+
+    const buttonElement = screen.getByRole('button')
+
+    await user.click(buttonElement)
+
+    expect(onClick).toBeCalledTimes(NEVER_CALLED)
   })
 })
