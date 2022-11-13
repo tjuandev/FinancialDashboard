96527960d4fdec80596dad10d35f1eeed904a4dd
tjuandev <tpydeveloper@gmail.com>
2022-11-13 17:26:18 -0300

    fix: Type error for custom TanStack table

--- src/components/molecules/Table/types.ts
+++ src/components/molecules/Table/types.ts
@@ -1,6 +1,10 @@
 import { ColumnDef } from '@tanstack/react-table'
 
+// Any used because of type error in TanStack library, see https://github.com/TanStack/table/issues/4241.
+// The issue is still open in the date of this comment (11-13-2022)
+
 export type TableProps<ColumnType> = {
   rows: ColumnType[]
-  columns: ColumnDef<ColumnType, string | number>[]
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  columns: ColumnDef<ColumnType, any>[]
 }
