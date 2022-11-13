2bdf3f5a16e36b941e3d656de20545079efbdf14
tjuandev <tpydeveloper@gmail.com>
2022-11-13 17:08:12 -0300

    feat: Improving table component

--- src/components/molecules/Table/index.tsx
+++ src/components/molecules/Table/index.tsx
@@ -1,6 +1,55 @@
-import * as S from './styles'
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable
+} from '@tanstack/react-table'
+
+import { useState } from 'react'
+
 import { TableProps } from './types'
 
-const Table = (props: TableProps) => <S.Container {...props}>Table</S.Container>
+const Table = <ColumnType,>(props: TableProps<ColumnType>) => {
+  const { columns, rows } = props
+
+  const [data] = useState([...rows])
+
+  const table = useReactTable<ColumnType>({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default Table
--- src/components/molecules/Table/types.ts
+++ src/components/molecules/Table/types.ts
@@ -1,3 +1,6 @@
-export type TableProps = {
-  name: string
+import { ColumnDef } from '@tanstack/react-table'
+
+export type TableProps<ColumnType> = {
+  rows: ColumnType[]
+  columns: ColumnDef<ColumnType, string | number>[]
 }
