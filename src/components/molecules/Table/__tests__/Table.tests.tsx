aab00dc10f0408a2849bffa268ae7f4bd9301afd
tjuandev <tpydeveloper@gmail.com>
2022-11-15 18:13:53 -0300

    refactor: Improving tests of Table

--- src/components/molecules/Table/__tests__/Table.tests.tsx
+++ src/components/molecules/Table/__tests__/Table.tests.tsx
@@ -3,11 +3,11 @@ import { generateTableColumns } from 'helpers/table'
 
 import Table from '..'
 
-type TestTable = {
+type TestTableColumns = {
   column: string
 }
 
-const { accessor } = generateTableColumns<TestTable>()
+const { accessor } = generateTableColumns<TestTableColumns>()
 
 const columns = [
   accessor('column', {
@@ -15,21 +15,65 @@ const columns = [
   })
 ]
 
-const rows: TestTable[] = [
+const rows: TestTableColumns[] = [
   {
-    column: 'row'
+    column: 'rowContent'
   }
 ]
 
-describe('<Table />', () => {
+describe('<Table />a', () => {
   it('Should render column and row', () => {
-    render(<Table<TestTable> columns={columns} rows={rows} />)
+    render(<Table<TestTableColumns> columns={columns} rows={rows} />)
 
     const columnElement = screen.getByRole('columnheader')
 
     const rowDataElement = screen.getByRole('cell')
 
     expect(columnElement).toHaveTextContent('column')
-    expect(rowDataElement).toHaveTextContent('row')
+    expect(rowDataElement).toHaveTextContent('rowContent')
+  })
+
+  it('Should apply width and textAlign to column based on columnExtension', () => {
+    render(
+      <Table<TestTableColumns>
+        columns={columns}
+        rows={rows}
+        columnsExtensions={[
+          {
+            id: 'column',
+            width: '150px',
+            horizontalAlign: 'center'
+          }
+        ]}
+      />
+    )
+
+    const columnElement = screen.getByRole('columnheader')
+
+    expect(columnElement).toHaveStyle({
+      width: '150px',
+      textAlign: 'center'
+    })
+  })
+
+  it("Should apply textAlign to row based on columnExtension of it's column", () => {
+    render(
+      <Table<TestTableColumns>
+        columns={columns}
+        rows={rows}
+        columnsExtensions={[
+          {
+            id: 'column',
+            horizontalAlign: 'center'
+          }
+        ]}
+      />
+    )
+
+    const rowElement = screen.getByRole('cell')
+
+    expect(rowElement).toHaveStyle({
+      textAlign: 'center'
+    })
   })
 })
