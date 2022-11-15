beda7c8cf3c973bff967dbd4dde342a20b86b84b
tjuandev <tpydeveloper@gmail.com>
2022-11-15 12:50:41 -0300

    feat: Improving styles of Table

--- src/components/molecules/Table/styles.tsx
+++ src/components/molecules/Table/styles.tsx
@@ -1,3 +1,48 @@
 import styled from 'styled-components'
+import theme from 'theme'
 
-export const Container = styled.div``
+export const Table = styled.table`
+  width: 100%;
+
+  border-collapse: collapse;
+
+  thead {
+    background: ${theme.colors.primary};
+    border-radius: ${theme.borderRadius.small};
+  }
+
+  tbody {
+    background: ${theme.colors.secondary};
+  }
+
+  th,
+  td {
+    font-size: 14px;
+  }
+
+  td {
+    padding: 0 16px;
+  }
+
+  th {
+    color: ${theme.colors.tertiary};
+    padding: 10px 16px;
+    text-align: left;
+  }
+
+  th:first-child {
+    border-radius: 16px 0 0 0;
+  }
+
+  th:last-child {
+    border-radius: 0 16px 0 0;
+  }
+
+  td:first-child {
+    border-radius: 0 0 0 16px;
+  }
+
+  td:last-child {
+    border-radius: 0 0 16px 0;
+  }
+`
