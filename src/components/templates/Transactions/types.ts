c5d78dd6e6f449a46807f34c13a907f0b9f8834d
tjuandev <tpydeveloper@gmail.com>
2022-11-16 21:16:25 -0300

    refactor: Removing children props

--- src/components/templates/Transactions/types.ts
+++ src/components/templates/Transactions/types.ts
@@ -1,7 +1,6 @@
-import { ComponentProps, ReactNode } from 'react'
+import { ComponentProps } from 'react'
 import { Header } from 'components/organisms'
 
 export type Props = {
   headerProps: ComponentProps<typeof Header>
-  children?: ReactNode
 }
