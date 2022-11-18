acc14f1f1f06244904a752162508ce59457bfd0d
tjuandev <tpydeveloper@gmail.com>
2022-11-17 21:31:16 -0300

    fix: improving uuid mock

--- src/components/templates/Transactions/__tests__/Transactions.test.tsx
+++ src/components/templates/Transactions/__tests__/Transactions.test.tsx
@@ -4,7 +4,21 @@ import Transactions from '..'
 
 jest.mock('uuid')
 
-// NOTE Improve test
+// NOTE Move it to testHelper after
+
+jest.mock('uuid', () => {
+  const base = '9134e286-6f71-427a-bf00-'
+  let current = 100000000000
+
+  return {
+    v4: () => {
+      const uuid = base + current.toString()
+      current++
+
+      return uuid
+    }
+  }
+})
 
 describe('<Transactions />', () => {
   it('Should match snapshot', () => {
