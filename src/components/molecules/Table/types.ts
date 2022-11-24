c0fb18969f2944b7849fccb89c8f9036d641f99c
tjuandev <tpydeveloper@gmail.com>
2022-11-24 19:58:04 -0300

    refactor: use of any type

--- src/components/molecules/Table/types.ts
+++ src/components/molecules/Table/types.ts
@@ -1,6 +1,4 @@
 import { ColumnDef, Table } from '@tanstack/react-table'
-// Any used because of type error in TanStack library, see https://github.com/TanStack/table/issues/4241.
-// The issue is still open in the date of this comment (11-13-2022)
 
 export type ColumnExtension = {
   id?: string
@@ -12,8 +10,7 @@ export type ColumnExtensions = ColumnExtension[]
 
 export type TableProps<ColumnType> = {
   rows: ColumnType[]
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  columns: ColumnDef<ColumnType, any>[]
+  columns: ColumnDef<ColumnType, unknown>[]
   columnsExtensions?: ColumnExtensions
 }
 
