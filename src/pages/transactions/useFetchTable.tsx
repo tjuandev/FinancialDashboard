333c260fe6842573756f6ed0368655b1b240e7cc
tjuandev <tpydeveloper@gmail.com>
2022-11-24 20:04:54 -0300

    refactor: removing unused variables

--- src/pages/transactions/useFetchTable.tsx
+++ src/pages/transactions/useFetchTable.tsx
@@ -2,14 +2,8 @@ import { useEffect } from 'react'
 import axios from 'axios'
 
 import { createColumnHelper } from '@tanstack/react-table'
-import { Button, Icon } from 'components/atoms'
-
-import theme from 'theme'
-import { css } from 'styled-components'
 
 import makeServer from './server'
-import { ColumnActionsContainer } from './styles'
-
 import { useQuery } from '@tanstack/react-query'
 
 import {
@@ -19,81 +13,29 @@ import {
   UseFetchTable
 } from './types'
 
-const columnHelper = createColumnHelper<TransactionsTable>()
+/* const actionButtonExtraStyles = css`
+width: 40px;
+height: 40px;
 
-const actionButtonExtraStyles = css`
-  width: 40px;
-  height: 40px;
+border-radius: ${theme.borderRadius.base};
 
-  border-radius: ${theme.borderRadius.base};
-
-  :hover {
-    filter: brightness(0.9);
-  }
+:hover {
+  filter: brightness(0.9);
+}
 `
 
-export const columns = [
-  columnHelper.accessor('type', {
-    header: 'Type',
-    cell: (info) => info.getValue()
-  }),
-  columnHelper.accessor('date', {
-    header: 'Date',
-    cell: (info) => info.getValue()
-  }),
-  columnHelper.accessor('description', {
-    header: 'Description',
-    cell: (info) => info.getValue().value
-  }),
-  columnHelper.accessor('value', {
-    header: 'Value',
-    cell: (info) => info.getValue()
-  }),
-  columnHelper.display({
-    id: 'actions',
-    header: 'Actions',
-    cell: () => (
-      <ColumnActionsContainer>
-        <Button padding="0.25rem" extraStyles={actionButtonExtraStyles}>
-          <Icon name="outlined-trash" />
-        </Button>
-        <Button padding="0.25rem" extraStyles={actionButtonExtraStyles}>
-          <Icon name="outlined-edit" />
-        </Button>
-      </ColumnActionsContainer>
-    )
-  })
-]
+const ActionButtons = () => (
+  <ColumnActionsContainer>
+  <Button padding="0.25rem" extraStyles={actionButtonExtraStyles}>
+  <Icon name="outlined-trash" />
+  </Button>
+  <Button padding="0.25rem" extraStyles={actionButtonExtraStyles}>
+  <Icon name="outlined-edit" />
+  </Button>
+  </ColumnActionsContainer>
+  ) */
 
-export const rows: TransactionsTable[] = [
-  {
-    type: 'Despesa',
-    date: '2022-02-2004',
-    description: {
-      value: 'Compras no submundo pós apocalíptico de Gânador',
-      columnWidth: '100px'
-    },
-    value: '2000'
-  },
-  {
-    type: 'Despesa',
-    date: '2022-02-2004',
-    description: {
-      value: 'Compras no submundo pós apocalíptico de Gânador',
-      columnWidth: '100px'
-    },
-    value: '2000'
-  },
-  {
-    type: 'Despesa',
-    date: '2022-02-2004',
-    description: {
-      value: 'Compras no submundo pós apocalíptico de Gânador',
-      columnWidth: '100px'
-    },
-    value: '2000'
-  }
-]
+const columnHelper = createColumnHelper<TransactionsTable>()
 
 const useFetchTable = (): UseFetchTable => {
   useEffect(() => {
