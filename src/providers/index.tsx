b3480b19d076e9004a7faacb2e0ac999d13009ea
tjuandev <tpydeveloper@gmail.com>
2022-11-24 19:58:33 -0300

    feat: configuring dev tools to providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,13 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
 
 const client = new QueryClient()
 
 const Providers = ({ children }: { children: ReactNode }) => (
-  <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  <QueryClientProvider client={client}>
+    <ReactQueryDevtools initialIsOpen={false} />I{children}
+  </QueryClientProvider>
 )
 
 export default Providers
