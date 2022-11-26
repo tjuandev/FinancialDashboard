123af030bf64a7966e6faa4f41b5d9da5e20e7fd
tjuandev <tpydeveloper@gmail.com>
2022-11-26 12:03:50 -0300

    refactor: putting uuid mock into tests/mocks

--- src/components/templates/Transactions/__tests__/Transactions.test.tsx
+++ src/components/templates/Transactions/__tests__/Transactions.test.tsx
@@ -1,24 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import 'tests/mocks/uuid'
+
 import { render } from '@testing-library/react'
 import Transactions from '..'
-
-jest.mock('uuid')
-
-// NOTE Move it to testHelper after
-
-jest.mock('uuid', () => {
-  const base = '9134e286-6f71-427a-bf00-'
-  let current = 100000000000
-
-  return {
-    v4: () => {
-      const uuid = base + current.toString()
-      current++
-
-      return uuid
-    }
-  }
-})
 
 describe('<Transactions />', () => {
   it('Should match snapshot', () => {
